96fd92fc00f08e56727606986e2a47e1d4fb1e71
Rutuja Lomate <104217802+Rutujalomate@users.noreply.github.com>
2022-12-22 16:32:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-const data = require('./db.json');
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(data);
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+//require("dotenv").config();
+///const PORT=process.env.PORT
+//console.log(PORT)
+const mongoose=require("mongoose")
+const express=require("express")
+const cors=require("cors")
+const app=express()
 
-server.use(middlewares);
-server.use(router);
-server.listen(port, () => console.log(`JSON Server is running on port ${port}`));
+const main=require("./src/config/db")
+//console.log(connect)
+app.use(express.json())
+app.use(cors())
+
+//console.log(POR)
+
+
+
+const userRoute=require("./src/features/user/user.router")
+
+//const productRoute=require("./features/product/product.router")
+
+const cartRoute=require("./src/features/blog/blog.router")
+
+app.use("/users",userRoute)
+//app.use("/products",productRoute)
+app.use("/blogs",cartRoute)
+
+
+app.listen(8080,async()=>{
+    try{
+       await main()
+   
+    }catch(e){
+   console.log(e.message)
+    }
+   
+       console.log(`port running on http://localhost:8080`)
+   })
+
+   // mongoimport --drop --db masaizon --collection users --file ./users.json --jsonArray
+   // {
+  
+  
+  //"name": "naio",
+  
+  //"email": "sdominique0@usda.gov",
+ // "password": "male",
+
+ // "age": 37
+//   }{
+
+  //"quantity":5,
+  //"user":"634b8b2b0a11c9f0e75d6fba",
+  //"product":"634b92bafbebc4dec1d9ef92"
+  //}
